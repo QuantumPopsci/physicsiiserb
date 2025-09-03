2bded70a2a40be66443ad168ee287431e39c28b2
Samriddha Ganguly <117953263+QuantumPopsci@users.noreply.github.com>
2025-09-04 00:43:37 +0530

    Create resourceData.js

---
+++ src/data/resourceData.js
@@ -0,0 +1,26 @@
+export const resources = {
+  articles: [
+    {
+      title: 'The Feynman Lectures on Physics',
+      description: 'A timeless collection of lectures by Richard Feynman.',
+      link: 'https://www.feynmanlectures.caltech.edu/',
+      type: 'web'
+    },
+    {
+      title: 'Gravitational Waves Detected 100 Years After Einstein\'s Prediction',
+      description: 'The landmark paper from the LIGO collaboration.',
+      link: 'https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.116.061102',
+      type: 'pdf'
+    },
+  ],
+  videos: [
+    {
+      title: 'What is Quantum Mechanics?',
+      videoId: 'q_qgVn-b_c0'
+    },
+    {
+      title: 'The Map of Physics',
+      videoId: 'Zihywtbsnp0'
+    }
+  ]
+};
