c821eb94c90069ed7134c00e1f6dd0b2c9f6df8d
Samriddha Ganguly <117953263+QuantumPopsci@users.noreply.github.com>
2025-09-04 00:41:05 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Courses from './pages/Courses';
+import CourseDetail from './pages/CourseDetail';
+import Resources from './pages/Resources';
+import Contacts from './pages/Contacts';
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-900">
+      <Navbar />
+      <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/courses/:courseId" element={<CourseDetail />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
