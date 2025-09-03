ddde8ae5475fc1cd127a3c25206cc7324dda1f92
Samriddha Ganguly <117953263+QuantumPopsci@users.noreply.github.com>
2025-09-04 00:29:06 +0530

    Create Footer.js

---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,13 @@
+// src/components/layouts/Footer.js
+import React from 'react';
+import './Layouts.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 Physics Majors, IISER Bhopal. Don't Panic! 🌌</p>
+    </footer>
+  );
+}
+
+export default Footer;
