7079da7df285ec3a025aafe1add66be52db4382b
Samriddha Ganguly <117953263+QuantumPopsci@users.noreply.github.com>
2025-09-04 01:43:52 +0530

    Update resourceData.js

--- src/data/resourceData.js
+++ src/data/resourceData.js
@@ -12,15 +12,22 @@ export const resources = {
       link: 'https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.116.061102',
       type: 'pdf'
     },
+    {
+      title: 'IBM Quantum Learning',
+      description: 'Kickstart your quantum learning journey with a selection of courses designed to help you learn the basics or explore more focused topics. If you’re an instructor, explore content specifically tailored to incorporating quantum in the classroom.',
+      link: 'https://quantum.cloud.ibm.com/learning/en',
+      type: 'web'
+    },
+    
   ],
   videos: [
     {
-      title: 'What is Quantum Mechanics?',
-      videoId: 'q_qgVn-b_c0'
+      title: 'Something strange happens when you trust Quantum Mechanics',
+      videoId: 'qJZ1Ez28C-A'
     },
     {
-      title: 'The Map of Physics',
-      videoId: 'Zihywtbsnp0'
+      title: 'Maxwell, Divergence and Fields',
+      videoId: 'rB83DpBJQsE'
     }
   ]
 };
